c47d3231b070f335f44af6608efda295088deca4
Pierre Rochette <pierre.rochette@epitech.eu>
2023-10-05 14:56:00 +0200

    Task 4 update

--- task04.php
+++ task04.php
@@ -5,15 +5,23 @@ function whoami() {
     $age = $_POST["age"] ?? null; 
 
     if (empty($name)) {
+
         if(empty($age)) {
+
             echo "Hi, I have no name."; 
         } else {
+
             echo "Hi, I have no name and I'm $age years old."; 
         }
+
     } else {
+
         if(!is_numeric($age)) {
+
             echo "Hi, my name is $name."; 
+
         } else {
+            
             echo "Hi, my name is $name and I'm $age years old."; 
         }
     }
