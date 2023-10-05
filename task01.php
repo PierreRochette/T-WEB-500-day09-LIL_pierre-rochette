72d4ece6cf6844ff3f2ceb2d096e271b85f10c30
Pierre Rochette <pierre.rochette@epitech.eu>
2023-10-05 13:55:05 +0200

    Update task 1

--- task01.php
+++ task01.php
@@ -1,17 +1,12 @@
 <?php
 
 function display_menu() {
-    $menuItems = array(
-        "Home" => "home.php", 
-        "Products" => "product.php", 
-        "About Us" => "about.php", 
-        "Contact" => "contact.php"
-    ); 
-
-    $menuHTML = "<ul>"; 
-    foreach($menuItems as $itemName => $itemLink) {
-        $menuHTML .= '<li><a href="' . $itemLink . '">' . $itemName . '</a></li>';
-    }
-    $menuHTML .= '</ul>'; 
-    return $menuHTML;
+    return <<<HTML
+        <ul>
+            <li><a href="home.php">Home</a></li>
+            <li><a href="product.php">Products</a></li>
+            <li><a href="about.php">About Us</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+        HTML; 
 }
