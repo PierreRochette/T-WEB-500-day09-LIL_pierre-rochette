98c3661248373ee972234755e77d20468edee3c5
Pierre Rochette <pierre.rochette@epitech.eu>
2023-10-05 11:28:02 +0200

    test

---
+++ task01.php
@@ -0,0 +1,17 @@
+<?php
+
+function display_menu() {
+    $menuItems = array(
+        "Home" => "home.php", 
+        "Products" => "product.php", 
+        "About Us" => "about.php", 
+        "Contact" => "contact.php"
+    ); 
+
+    $menuHTML = "<ul>"; 
+    foreach($menuItems as $itemName => $itemLink) {
+        $menuHTML .= '<li href="' . $itemLink . '">' . $itemName . '</a></li';
+    }
+    $menuHTML .= '</ul>'; 
+    return $menuHTML;
+}
